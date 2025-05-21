9b6f6509486981eed31f130391e103df5c523759
iamamemberofarobloxscript <johnaaronpalamos@gmail.com>
2025-05-21 19:51:28 +0800

    Create Mygui.lua

---
+++ Mygui.lua
@@ -0,0 +1,10 @@
+
+getgenv().BondsFarm = true
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/LiterallyBacon12/LUNEC-On-Top/refs/heads/main/Official%20Source.lua'))()
+
+
+task.delay(5, function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LiterallyBacon12/LUNEC-On-Top/refs/heads/main/Official%20Source.lua'))()
+end)
